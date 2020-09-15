d6370cd23f870f47854c5a658e052837a3bd317f
Andrew Ng <ngandrew1337@gmail.com>
2020-09-15 02:27:56 -0800

    msarray.h: all explanatory comments and invariants

--- Project02/msarray.h
+++ Project02/msarray.h
@@ -1,42 +1,70 @@
 // msarray.h
-// Andrew Ng
+// Andrew S. Ng
 // 2020-09-14
 //
 // CS 311 Fall 2020
 // Header for class template MSArray
-// Moderately Smart Array:
-//   fixed-size array of generic type
+// RAII class holding array of generic type and its size
 
 #ifndef FILE_MSARRAY_H_INCLUDED
 #define FILE_MSARRAY_H_INCLUDED
 
-#include <cstddef>    
-#include <algorithm>  
-#include <utility>    
+#include <cstddef>    // for std::size_t
+#include <algorithm>  // for generic algorithms
+#include <utility>    // std::swap
 
 
-template <typename ValType>
+/////////////////////////////////////////////////////
+//    class template MSArray - Class definition    //
+/////////////////////////////////////////////////////
+
+
+// class MSArray
+// RAII class holding array of generic type and its size
+// Invariants:
+//   _arrayptr points to memory allocated by new[], owned by *this,
+//     and with enough space for _size value_type objects.
+//   _size >= 0
+// Requirements on Types:
+//   T must have a copy ctor.
+template <typename T>
 class MSArray
 {
 
+// ---- MSArray: public member types ----
 public:
 
-    using size_type = std::size_t;
+    using size_type = std::size_t;  // Unsigned type for
+                                    // array size and indices.
+    using value_type = T;           // Type of items in container.
 
-    using value_type = ValType;
-
+// ---- MSArray: ctors, dctors, op= ----
 public:
 
+    // Default ctor
+    // Allocates array of size 8.
+    // All items default-constructed.
     MSArray()
         :_arrayptr(new value_type[8]),
          _size(8)
     {}
 
+    // Ctor from size
+    // Allocates array of given size.
+    // All items default-constructed.
+    // Not an implicit type conversion.
+    // Pre:
+    //   size >= 0
     explicit MSArray(size_type size)
         :_arrayptr(new value_type[size]),
          _size(size)
     {}
 
+    // Ctor from size and value
+    // Allocates array of given size.
+    // All items filled with given value.
+    // Pre:
+    //   size >= 0
     MSArray(size_type size,
             const value_type & value)
         :_arrayptr(new value_type[size]),
@@ -45,11 +73,16 @@ public:
         std::fill(begin(), end(), value);
     }
 
+    // Big 5: all defined
+
+    // Dctor
+    // Deallocates array
     ~MSArray() noexcept
     {
         delete [] _arrayptr;
     }
 
+    // Copy ctor
     MSArray(const MSArray<value_type> & other)
         :_arrayptr(new value_type[other.size()]),
          _size(other.size())
@@ -57,6 +90,7 @@ public:
         std::copy(other.begin(), other.end(), begin());
     }
 
+    // Move ctor
     MSArray(MSArray<value_type> && other) noexcept
         :_arrayptr(other._arrayptr),
          _size(other.size())
@@ -64,6 +98,7 @@ public:
         other._arrayptr = nullptr;
     }
 
+    // Copy assignment
     MSArray<value_type> & operator=(const MSArray<value_type> & rhs)
     {
         MSArray<value_type> copy(rhs);
@@ -71,71 +106,96 @@ public:
         return *this;
     }
     
+    // Move assignment
     MSArray<value_type> & operator=(MSArray<value_type> && rhs) noexcept
     {
         swap(rhs);
         return *this;
     }
 
+// ---- MSArray: general public functions ----
 public:
 
+    // size
+    // Returns number of items in array.
     size_type size() const
     {
         return _size;
     }
 
+    // begin - non-const & const
+    // Returns address of item 0 in array.
     value_type * begin()
     {
         return _arrayptr;
     }
-
     const value_type * begin() const
     {
         return _arrayptr;
     }
 
+    // end - non-const & const
+    // Returns address of item one-past
+    // the end of the array.
     value_type * end()
     {
         return begin() + size();
     }
-
     const value_type * end() const
     {
         return begin() + size();
     }
 
+// ---- MSArray: general public operators ----
 public:
 
+    // op[] - non-const & const
+    // Returns a reference to the item at the index.
+    // Pre:
+    //   0 <= index < _size
     value_type & operator[](size_type index)
     {
         return _arrayptr[index];
     }
-
     const value_type & operator[](size_type index) const
     {
         return _arrayptr[index];
     }
 
+// ---- MSArray: general private functions ----
 private:
 
+    // swap
+    // Swaps values of this object and other object.
     void swap(MSArray<value_type> & other) noexcept
     {
         std::swap(_arrayptr, other._arrayptr);
         std::swap(_size, other._size);
     }
 
+// ---- MSArray: data members ----
 private:
 
-    value_type * _arrayptr;
+    value_type * _arrayptr;  // Pointer to dynamic array
 
-    size_type _size;
+    size_type _size;         // Size of array (num. of items)
 
-};
+};  // End class MSArray
 
 
-template <typename ValType>
-bool operator==(const MSArray<ValType> & lhs,
-                const MSArray<ValType> & rhs)
+////////////////////////////////////////////////////////////////
+//    class template MSArray - Global operator definitions    //
+////////////////////////////////////////////////////////////////
+
+
+// op== (MSArray<T>, MSArray<T>)
+// Returns true if sizes are equal,
+// and all items are equal.
+// Requirements on Types:
+//   op== (T, T) must exist.
+template <typename T>
+bool operator==(const MSArray<T> & lhs,
+                const MSArray<T> & rhs)
 {
     if (lhs.size() == rhs.size()) {
         return std::equal(lhs.begin(),
@@ -146,17 +206,27 @@ bool operator==(const MSArray<ValType> & lhs,
 }
 
 
-template <typename ValType>
-bool operator!=(const MSArray<ValType> & lhs,
-                const MSArray<ValType> & rhs)
+// op!= (MSArray<T>, MSArray<T>)
+// Returns true if both MSArray<T>
+// are not equal
+// Requirements on Types:
+//   op== (T, T) must exist.
+template <typename T>
+bool operator!=(const MSArray<T> & lhs,
+                const MSArray<T> & rhs)
 {
     return !(lhs == rhs);
 }
 
 
-template <typename ValType>
-bool operator<(const MSArray<ValType> & lhs,
-                const MSArray<ValType> & rhs)
+// op< (MSArray<T>, MSArray<T>)
+// Returns true if lhs is
+// lexicographically less than rhs
+// Requirements on Types:
+//   op< (T, T) must exist.
+template <typename T>
+bool operator<(const MSArray<T> & lhs,
+                const MSArray<T> & rhs)
 {
     return std::lexicographical_compare(lhs.begin(),
                                         lhs.end(),
@@ -165,28 +235,39 @@ bool operator<(const MSArray<ValType> & lhs,
 }
 
 
-template <typename ValType>
-bool operator>(const MSArray<ValType> & lhs,
-                const MSArray<ValType> & rhs)
+// op> (MSArray<T>, MSArray<T>)
+// Returns true if rhs is less than lhs
+// Requirements on Types:
+//   op< (T, T) must exist.
+template <typename T>
+bool operator>(const MSArray<T> & lhs,
+                const MSArray<T> & rhs)
 {
     return rhs < lhs;
 }
 
 
-template <typename ValType>
-bool operator<=(const MSArray<ValType> & lhs,
-                const MSArray<ValType> & rhs)
+// op<= (MSArray<T>, MSArray<T>)
+// Returns true if rhs is not less than lhs
+// Requirements on Types:
+//   op< (T, T) must exist.
+template <typename T>
+bool operator<=(const MSArray<T> & lhs,
+                const MSArray<T> & rhs)
 {
     return !(rhs < lhs);
 }
 
 
-template <typename ValType>
-bool operator>=(const MSArray<ValType> & lhs,
-                const MSArray<ValType> & rhs)
+// op>= (MSArray<T>, MSArray<T>)
+// Returns true if lhs is not less than rhs
+// Requirements on Types:
+//   op< (T, T) must exist.
+template <typename T>
+bool operator>=(const MSArray<T> & lhs,
+                const MSArray<T> & rhs)
 {
     return !(lhs < rhs);
 }
 
-
-#endif // FILE_MSARRAY_H_INCLUDED
+#endif // #ifndef FILE_MSARRAY_H_INCLUDED
