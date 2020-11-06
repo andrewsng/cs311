bb2a0bed63d4b56c2f86bf952d1284a5085ec3c4
Andrew Ng <ngandrew1337@gmail.com>
2020-11-05 16:38:29 -0900

    dp6.h: Documentation, improved erase()

--- Project06/dp6.h
+++ Project06/dp6.h
@@ -1,6 +1,6 @@
 // dp6.h
 // Andrew S. Ng
-// Started: 2020-11-05
+// Started: 2020-11-04
 // Updated: 2020-11-05
 //
 // For CS 311 Fall 2020
@@ -17,6 +17,11 @@
 #include <functional> // For std::function
 
 
+// reverseList
+// Reverses the items in a given linked list.
+// Requirements on Types:
+//   None.
+// No-Throw Guarantee
 template <typename ValType>
 void reverseList(std::unique_ptr<LLNode2<ValType>> & head)
 {
@@ -30,10 +35,19 @@ void reverseList(std::unique_ptr<LLNode2<ValType>> & head)
 }
 
 
+// class template LLMap
+// Associative dataset of client-specified key and value types.
+// Invariants:
+//   _dataset is empty or points to an LLNode2 of key-value pairs.
+//   No two pairs have the same keys (equal by op==).
+// Requirements on Types:
+//   KeyType and ValType have a copy ctor and a non-throwing dctor.
+//   KeyType has the equality operator (==).
 template <typename KeyType, typename ValType>
 class LLMap
 {
 
+// ---- LLMap: public member types ----
 public:
 
     using key_type = KeyType;
@@ -44,51 +58,75 @@ public:
 
     using size_type = std::size_t;
 
-    using iterator = value_type *;
-    using const_iterator = const value_type *;
-
+// ---- LLMap: ctors, dctors, op= ----
 public:
 
+    // Default ctor
+    // No-Throw Guarantee
     LLMap() = default;
 
+    // Dctor
+    // No-Throw Guarantee
     ~LLMap() = default;
 
+    // No copy/move operations
     LLMap(const LLMap<key_type, value_type> & other) = delete;
     LLMap(LLMap<key_type, value_type> && other) = delete;
     LLMap<key_type, value_type> & operator=(const LLMap<key_type, value_type> & other) = delete;
     LLMap<key_type, value_type> & operator=(LLMap<key_type, value_type> && other) = delete;
 
+// ---- LLMap: general public functions ----
 public:
 
-    size_type size() const
+    // size
+    // Returns number of key-value pairs in dataset.
+    // No-Throw Guarantee
+    size_type size() const noexcept
     {
-        return ::size(_dataset);  // Refer to global function
+        return ::size(_dataset);  // Refer to global scope
     }
 
-    bool empty() const
+    // empty
+    // Returns true when dataset has no key-value pairs.
+    // No-Throw Guarantee
+    bool empty() const noexcept
     {
         return size() == 0;
     }
 
-    iterator find(const key_type & key)
+    // find - non-const & const
+    // Searches for given key within the dataset.
+    // If found, returns pointer to associated value,
+    //   otherwise returns nullptr.
+    // No-Throw Guarantee
+    value_type * find(const key_type & key) noexcept
     {
-        for (auto p = _dataset.get(); p != nullptr; p = p->_next.get())
+        auto p = _dataset.get();
+        while (p != nullptr)
         {
             if (key == p->_data.first)
                 return &(p->_data.second);
+            p = p->_next.get();
         }
         return nullptr;
     }
-    const_iterator find(const key_type & key) const
+    const value_type * find(const key_type & key) const noexcept
     {
-        for (auto p = _dataset.get(); p != nullptr; p = p->_next.get())
+        auto p = _dataset.get();
+        while (p != nullptr)
         {
             if (key == p->_data.first)
                 return &(p->_data.second);
+            p = p->_next.get();
         }
         return nullptr;
     }
 
+    // insert
+    // Inserts key-value pair into dataset if key does not already
+    //   exist, otherwise replaces existing pair.
+    // Strong Guarantee
+    // Exception Neutral
     void insert(const key_type & key, const value_type & value)
     {
         auto p = find(key);
@@ -98,27 +136,46 @@ public:
             push_front(_dataset, std::make_pair(key, value));
     }
 
-    void erase(const key_type & key)
+    // erase
+    // If key exists in the dataset, the key-value pair is removed.
+    // No-Throw Guarantee
+    void erase(const key_type & key) noexcept
     {
+        // Empty case
+        if (empty())
+            return;
+
+        // Check first pair
         if (key == _dataset->_data.first)
         {
             pop_front(_dataset);
             return;
         }
 
+        // Iterate: if next pair matches key, replace next pair
+        //   with whatever it's pointing to.
         auto p = _dataset.get();
-        while (p != nullptr)
+        while (p->_next)
         {
-            if (key == p->_next->_data.first)
+            auto & np = p->_next;
+            if (key == np->_data.first)
             {
-                p->_next = std::move(p->_next->_next);
+                auto temp = std::move(np->_next);
+                np = std::move(temp);
                 return;
             }
-            p = p->_next.get();
+            p = np.get();
         }
     }
 
-    void traverse(const std::function<void(key_type, value_type)> & f)
+    // traverse
+    // Passed function is called on each key-value pair in dataset.
+    // Pre:
+    //   Function f has two parameters: (key type, value type).
+    //   Function f returns nothing.
+    // Strong Guarantee
+    // Exception Neutral
+    void traverse(const std::function<void(key_type, value_type)> & f) const
     {
         auto p = _dataset.get();
         while (p != nullptr)
@@ -128,11 +185,13 @@ public:
         }
     }    
 
+// ---- LLMap: data members ----
 private:
 
+    // Associative dataset
     std::unique_ptr<LLNode2<pair_type>> _dataset;
 
-};
+};  // End class LLMap
 
 
 #endif  // #ifndef FILE_DP6_H_INCLUDED
